c66a09efb96c66fffdf9b5bba12abc5614bcdbf1
hoffmann <silash169@gmail.com>
2023-12-20 18:34:38 +0100

    implemented rough outline for learning ui

--- wotd-frontend/src/components/trainer/Trainer.tsx
+++ wotd-frontend/src/components/trainer/Trainer.tsx
@@ -6,6 +6,7 @@ import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Card from 'react-bootstrap/Card';
 import {Col, Row} from "react-bootstrap";
 import ListGroup from 'react-bootstrap/ListGroup';
+import Stack from 'react-bootstrap/Stack';
 
 
 export function Trainer() {
@@ -14,16 +15,55 @@ export function Trainer() {
     return (
         <div>
             <Container fluid="md">
-                <div className="custom-max-width">
+                <div className="custom-max-width custom-min-height debugborderpurple">
 
-                    <Card border='light' className=''>
-                        <ListGroup variant="flush">
-                            <ListGroup.Item>Front</ListGroup.Item>
-                            <ListGroup.Item>Back</ListGroup.Item>
-                        </ListGroup>
+                    {/*<div className="flexitem">test1</div>*/}
+                    {/*<div className="flexitem">test2</div>*/}
+
+                    {/*/!*<Card border='dark' className='flexcontainer'>*!/*/}
+                    {/*    <ListGroup variant="flush" className=''>*/}
+                    {/*        <ListGroup.Item className='flexitem'>Front</ListGroup.Item>*/}
+                    {/*        <ListGroup.Item>Back</ListGroup.Item>*/}
+                    {/*    </ListGroup>*/}
 
 
-                        <div className='min-height'>
+                    {/*    <div className='min-height'>*/}
+                    {/*        <ButtonGroup className='w-100' aria-label="Basic example">*/}
+                    {/*            <Button variant="light text-muted">Very Hard</Button>*/}
+                    {/*            <Button variant="light text-muted">Hard</Button>*/}
+                    {/*            <Button variant="light text-muted">Easy</Button>*/}
+                    {/*            <Button variant="light text-muted">Very Easy</Button>*/}
+                    {/*        </ButtonGroup>*/}
+                    {/*    </div>*/}
+                    {/*/!*</Card>*!/*/}
+
+                    {/*<Stack direction="vertical" className="debugbordergreen h-100" gap={3}>*/}
+                    {/*    <div className="debugbordergreen">First item</div>*/}
+                    {/*    <div className="debugborderpurple mh-100">Second item</div>*/}
+                    {/*    <div className="vr"/>*/}
+                    {/*    <div className="debugborderpurple">Third item</div>*/}
+                    {/*</Stack>*/}
+
+                    {/*<div className='debugborder '>*/}
+                    {/*    /!*<div className='debugborder d-flex flex-column'>*!/*/}
+                    {/*    <div className='debugborderpurple flexitem'>1</div>*/}
+                    {/*    <div>2</div>*/}
+                    {/*</div>*/}
+
+                    <div className="d-flex flex-column" style={{height: '500px'}}>
+                        <div className="" style={{height: '30px', backgroundColor: 'lightcoral'}}>
+                            {/* Content for the second child div goes here */}
+                            <p>This is the second child div.</p>
+                        </div>
+                        <div className="flex-grow-1" style={{backgroundColor: 'lightblue'}}>
+                            {/* Content for the first child div goes here */}
+                            <p>This is the first child div.</p>
+                        </div>
+                        {/* Second Child Div */}
+                        <div className="" style={{backgroundColor: 'lightcoral'}}>
+                            {/* Content for the second child div goes here */}
+                            {/*<p>This is the second child div.</p>*/}
+
                             <ButtonGroup className='w-100' aria-label="Basic example">
                                 <Button variant="light text-muted">Very Hard</Button>
                                 <Button variant="light text-muted">Hard</Button>
@@ -31,7 +71,7 @@ export function Trainer() {
                                 <Button variant="light text-muted">Very Easy</Button>
                             </ButtonGroup>
                         </div>
-                    </Card>
+                    </div>
 
 
                 </div>
